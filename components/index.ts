552853663ae568e0c860ad01326259492fa088fd
itsarasib <itsara.pliansart@gmail.com>
2024-03-01 20:28:00 +0700

    finish search bar

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,18 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
 
-export { Hero, CustomButton, NavBar, Footer };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+};
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,23 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturers: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
